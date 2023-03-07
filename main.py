2998387da4c1f528b3edea82d527f7a062fd01a4
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-03-07 21:27:52 +0200

    beta6

--- main.py
+++ main.py
@@ -2,71 +2,65 @@ import string
 import secrets
 import printer as pt
 import logging
+from art import text2art
 
 
 logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-class Password_Generator:
+class PasswordGenerator:
 
-    def __init__(self, password_symbols: str, password_lenght: int) -> None:
-        self.password_symbols = password_symbols # will hold the characters from which it will generate password
-        self.password_lenght = password_lenght # lenght of password
-        self.password = "" # Empty
+    def __init__(self, password_symbols: str, password_length: int) -> None:
+        self.password_symbols = password_symbols
+        self.password_length = password_length 
+        self.password = "" 
 
-    #function to get generated password
     def generate_password(self): 
-        for lenght  in range(self.password_lenght):
+        for length  in range(self.password_length):
             self.password += "".join(secrets.choice(self.password_symbols))
         return self.password
-
-    #function to get generated password    
+  
     def get_password(self): 
         return self.password
 
-#generates weak password with only letters
-class Password_With_Only_Letters(Password_Generator):
-    def __init__(self, lenght: int) -> None:
-        super().__init__(string.ascii_letters, lenght)
+class WeakPassword(PasswordGenerator):
+    def __init__(self, length: int) -> None:
+        super().__init__(string.ascii_letters, length)
 
-#generates avarage password with letters and digits
-class Password_With_Letters_And_Digits(Password_Generator):
-    def __init__(self, lenght: int) -> None:
-        super().__init__(string.ascii_letters + string.digits, lenght)
+class AveragePassword(PasswordGenerator):
+    def __init__(self, length: int) -> None:
+        super().__init__(string.ascii_letters + string.digits, length)
 
-#generates strong password with letters, digits and special symbols
-class Password_With_All_Symbols(Password_Generator):
-    def __init__(self, lenght: int) -> None:
-        super().__init__(string.ascii_letters + string.digits + string.punctuation, lenght)
+class StrongPassword(PasswordGenerator):
+    def __init__(self, length: int) -> None:
+        super().__init__(string.ascii_letters + string.digits + string.punctuation, length)
 
-    #overriding, taking function from Password_Generator class
     def generate_password(self):
         super().generate_password()
         self.password += ":)"
 
-while True:
-    print("------------------------------------------------------")
-    print("--> SECURE PASSWORD GENERATOR <-----------------------")
-    print("------------------------------------------------------")
-    print("1. Password only with letters")
-    print("2. Password with letters and digits")
-    print("3. Password with letters, digits and special symbols")
-    print("4. Exit program")
-    print("------------------------------------------------------")
+def get_input_choice() -> int:
+    while True:
+        try:
+            program_name=text2art("PASSWORD GENERATOR", font="smkeyboard")
+            print(program_name)
+            print("------------------------------------------------------")
+            print("1. Only letters")
+            print("2. Letters and digits")
+            print("3. Letters, digits, special symbols + smile")
+            print("4. Exit program")
+            print("------------------------------------------------------")
 
-    try:
-        choice = int(input("Enter your a number of your choice: "))
-        if choice > 4 or choice == 0:
-            print("\nInvalid number entered.\n")
+            inputed_choice = int(input("Enter a number of your choice: "))
+            if inputed_choice > 4 or inputed_choice == 0:
+                print("\nInvalid number entered.\n")
+                continue
+
+        except ValueError:
+            print("\nError: invalid input for menu choice.\n")
             continue
+        return inputed_choice
 
-    except ValueError:
-        print("\nError: invalid input for menu choice.\n")
-        continue
-
-    if choice == 4:
-        print("\nThank you for using password generator!")
-        break
-
+def get_password_length() -> int:
     while True:
         try:
             inputed_length = int(input("Enter password length: "))
@@ -77,20 +71,24 @@ while True:
         except ValueError:
             print("\nError: invalid input for password length.\n")
             continue
-        break   
-    
-    password = None
-        
-    if choice == 1:
-        password = Password_With_Only_Letters(inputed_length)
-    elif choice == 2:
-        password = Password_With_Letters_And_Digits(inputed_length)
-    elif choice == 3:
-        password = Password_With_All_Symbols(inputed_length)
+        return inputed_length
 
-    password.generate_password()
+if __name__ == "__main__":
+    while True:
+        input_choice = get_input_choice()
+        if input_choice == 4:
+            print("\nThank you for using password generator!")
+            break
 
-    pt.Console(f"\nGenerated password: {password.get_password()}").print_to_console()
-    pt.File("password.txt",password.get_password()).print_to_file()
+        password_length = get_password_length()
 
-    
+        if input_choice == 1:
+            password = WeakPassword(password_length)
+        elif input_choice == 2:
+            password = AveragePassword(password_length)
+        else:
+            password = StrongPassword(password_length)
+            
+        password.generate_password()
+        pt.Console(f"\nGenerated password: {password.get_password()}").print_to_console()
+        pt.File("trash.txt",password.get_password()).print_to_file()
